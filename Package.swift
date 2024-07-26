1a70e588459c513a9a1933f8c4373e9b9b921e98
Dave Carrigan <dave@openshut.net>
2024-07-26 09:38:05 -0700

    Track specific version of rxswift

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     .library(name: "RxSwiftExt", targets: ["RxSwiftExt"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/FountainheadMobileSolutions/RxSwift.git", branch: "topic/xcode16"),
+    .package(url: "https://github.com/FountainheadMobileSolutions/RxSwift.git", .upToNextMajor(from: "6.8.0")),
   ],
   targets: [
     .target(
